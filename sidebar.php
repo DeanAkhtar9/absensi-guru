d1e93a89e6fcbccef1092e744250bd68e9536b1b
DeanAkhtar9 <mdeanakhtar@gmail.com>
2026-02-19 16:17:55 +0700

    add

--- sidebar.php
+++ sidebar.php
@@ -14,16 +14,10 @@ $role = $_SESSION['role'];
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="/absensi-guru/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="/absensi-guru/assets/css/style.css">
-    <link rel="stylesheet" href="/absensi-guru/assets/css/responsive.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 </head>
 <body>
-<!-- Tombol Hamburger (Mobile Only) -->
-<button class="toggle-btn d-md-none" onclick="toggleSidebar()">
-    <i class="bi bi-list"></i>
-</button>
 
 <div class="sidebar">
 
@@ -59,15 +53,20 @@ $role = $_SESSION['role'];
         </li>
         <!-- ================= SISWA ================= -->
         <?php if($role == 'siswa'): ?>
-        <li>
-            <a href="/absensi-guru/siswa/buat_laporan.php">
-                <i class="bi bi-pencil-square"></i> Buat Laporan </a>
-        </li>
+            
+            <li>
+                <a href="/absensi-guru/siswa/buat_laporan.php">
+                    <i class="bi bi-send"></i>
+                    Buat Laporan
+                </a>
+            </li>
 
-        <li>
-            <a href="/absensi-guru/siswa/riwayat_laporan.php">
-                <i class="bi bi-file-earmark-text"></i> Riwayat Laporan </a>
-        </li>
+            <li>
+                <a href="/absensi-guru/siswa/riwayat_laporan.php">
+                    <i class="bi bi-clock-history"></i>
+                    Riwayat Laporan
+                </a>
+            </li>
 
         <?php endif; ?>
 
@@ -75,36 +74,26 @@ $role = $_SESSION['role'];
         <!-- ================= GURU ================= -->
         <?php if($role == 'guru'): ?>
 
-                <a href="/absensi-guru/guru/jadwal.php">
-                    <i class="bi bi-file-earmark-text"></i>
-                    Jadwal
-                </a>
-            </li>
             <li>
-                <a href="/absensi-guru/guru/rekap.php">
+                <a href="/absensi-guru/guru/laporan.php">
                     <i class="bi bi-file-earmark-text"></i>
-                    Rekap
+                    Laporan Masuk
                 </a>
             </li>
 
              <li>
                 <a href="/absensi-guru/guru/jurnal.php">
                     <i class="bi bi-check2-square"></i>
-                    Jurnal
+                    jurnal
                 </a>
             </li>
             <li>
-                <a href="/absensi-guru/guru/riwayat_absensi.php">
+                <a href="/absensi-guru/guru/konfirmasi.php">
                     <i class="bi bi-check2-square"></i>
-                    Riwayat Absensi
+                    Konfirmasi Laporan
                 </a>
             </li>
-            <li>
-                <a href="/absensi-guru/guru/riwayat_jurnal.php">
-                    <i class="bi bi-check2-square"></i>
-                    Riwayat Jurnal
-                </a>
-            </li>
+
         <?php endif; ?>
 
 
@@ -126,7 +115,7 @@ $role = $_SESSION['role'];
             </li>
 
             <li>
-                <a href="/absensi-guru/admin/kelas.php">
+                <a href="/absensi-guru/admin/siswa.php">
                     <i class="bi bi-people"></i>
                     Data Siswa
                 </a>
@@ -173,10 +162,3 @@ $role = $_SESSION['role'];
     </ul>
 
 </div>
-<script>
-function toggleSidebar() {
-    document.querySelector(".sidebar").classList.toggle("active");
-}
-</script>
-
-</body>
